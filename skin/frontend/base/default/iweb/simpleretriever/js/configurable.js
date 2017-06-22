632ea9637f2baf786fd16ac1d2a086b4ec9e8f0b
Dave Baker <dbaker@iweb.co.uk>
2017-06-22 10:23:04 +0100

    Fire event if no selection

--- skin/frontend/base/default/iweb/simpleretriever/js/configurable.js
+++ skin/frontend/base/default/iweb/simpleretriever/js/configurable.js
@@ -3,15 +3,15 @@
         Product.Config.prototype.configure = Product.Config.prototype.configure.wrap(function(parent, element){
             parent(element);
 
-            var products;
+            var products = [];
 
             for(var i=this.settings.length-1;i>=0;i--){
                 var selected = this.settings[i].options[this.settings[i].selectedIndex];
                 if(!selected.config){
-                    return;
+                    break;
                 }
 
-                if(!products){
+                if(!products.length){
                    products = selected.config.products.slice(0);
                 }else{
                     for(var c = 0; c < products.length; c++){
@@ -23,11 +23,14 @@
                 }
             }
 
+
             // We should have one simple to work with, which is our selected product
             if(products.length == 1){
                 if(this.config.simpleData && this.config.simpleData[products[0]]){
                     $(window.spConfig).trigger('simpleretriever_select_simple', this.config.simpleData[products[0]]);
                 }
+            }else{
+                $(window.spConfig).trigger('simpleretriever_select_none');
             }
 
         });
